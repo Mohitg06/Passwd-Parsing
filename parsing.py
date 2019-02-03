87989f3c532d4d4bc3f77032278b76f8a141a113
Mohit Gadkari <mohit.gadkari@teradata.com>
2019-02-02 16:39:44 -0800

    Test with method

--- parsing.py
+++ parsing.py
@@ -6,52 +6,60 @@ import re
 import json
 import csv
 
-class Parsing:
-    with open("./test_files/2/out5.txt", "w") as out_file:
-        with open("./test_files/2/passwd", "r") as in_file_99:
 
-            for line in in_file_99:
-                ki = line.strip()
-                if not ki.startswith("#"):  ## excluding first few comments in the file "passwd"
-                    key = line.split(':')
+class Parsing():
+    def __init__(self):
+        self.file_name = ("out8.csv")
+        self.path = ("./test_files/2/")
+        self.full_path = os.path.join(self.path,self.file_name)
+        self.file_name_open = open (self.full_path, "w")
 
-                    with open("./test_files/2/group", "r") as in_file_9:
-                        temp = 0
-                        for line in in_file_9:
 
-                            li = line.strip()
-                            if not li.startswith("#"):  ## excluding first few comments in the file "group"
 
-                                lock = line.split(':')
-                                if re.fullmatch(key[3], lock[
-                                    2]):  ## fullmatch keyword returns True for'1' match with '1' and False for 1' match with '10'
-                                    temp = +1
+    def file_open(self):
+        # self.path = open("./test_files/2/out8.csv", "w")
+
+        with self.file_name_open  as out_file:
+            with open("/etc/passwd", "r") as in_file_99:
+
+                out_file.write("username,uid,full_name,groups\n")
+                for line in in_file_99:
+                    ki = line.strip()
+                    if not ki.startswith("#"):  ## excluding first few comments in the file "passwd"
+                        key = line.split(':')
+
+                        with open("/etc/group", "r") as in_file_9:
+                            temp = 0
+                            for line in in_file_9:
+
+                                li = line.strip()
+                                if not li.startswith("#"):  ## excluding first few comments in the file "group"
+
+                                    lock = line.split(':')
+                                    if re.fullmatch(key[3], lock[
+                                        2]):  ## fullmatch keyword returns True for'1' match with '1' and False for 1' match with '10'
+                                        temp = +1
                                     out_file.write(
-                                        '"username": {0},"uid": {1},"full_name": {2},"groups": {3}'.format(key[0],
-                                                                                                           key[2],
-                                                                                                           key[4],
-                                                                                                           lock[3]))
-                        if temp == 0:
-                            out_file.write(
-                                '"username": {0},"uid": {1},"full_name": {2},ERROR: no info found for group id {3}\n'.format(
-                                    key[0], key[2], key[4], key[3]))
+                                    '{0},{1},{2},{3}'.format(key[0], key[2], key[4], lock[3]))
+                            if temp == 0:
+                                out_file.write(
+                                '{0},{1},{2},ERROR: no info found for group id {3}\n'.format(key[0], key[2], key[4],
+                                                                                             key[3]))
 
-    sys.stdout = open('output.json', 'w')
-    with open('./test_files/2/out5.txt', 'r') as csvfile:
-        filereader = csv.reader(csvfile, delimiter=',')
-        i = 0
-        header = []
-        out_data = []
-        for row in filereader:
-            row = [elem for elem in row if elem]
-            if i == 0:
-                i += 1
-                header = row
-            else:
-                row[0:2] = [row[0] + " " + row[1]]
-                _dict = {}
-                for elem, header_elem in zip(row, header):
-                    _dict[header_elem] = elem
-                out_data.append(_dict)
+        return self.file_name_open
 
-    print(json.dumps(out_data))
+# csvFilePath = ("./test_files/2/out8.csv")
+# jsonFilePath = ("./test_files/2/out8.json")
+#
+# data = {}
+#
+# with open(csvFilePath) as csvFile:
+#     csvReader = csv.DictReader(csvFile)
+#
+#     for csvRow in csvReader:
+#         username = csvRow["username"]
+#         data[username] = csvRow
+
+
+# with open(jsonFilePath, "w") as jsonFile:
+#     jsonFile.write(json.dumps(data))
--- tests/test_passwd_parsing.py
+++ tests/test_passwd_parsing.py
@@ -1,6 +1,15 @@
 
 from parsing import Parsing
 
+
+# def test_system_works(self):
+#     # Steps:
+#     #   1. verify that this tests fails due to assertion
+#     #   2. Make this test pass by changing False to True
+#     #   3. Delete this test
+#     self.assertTrue(True)
+
+
 def test_file_found():
     file = Parsing()
     assert file is  not None
